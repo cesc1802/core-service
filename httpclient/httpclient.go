f0e7a28594f28ef8544aa820be8b4cc5e99113d4
Cesc Nguyen <thuocnv1802@gmail.com>
2021-08-06 14:54:07 +0700

    http client

--- httpclient/httpclient.go
+++ httpclient/httpclient.go
@@ -1,1 +1,37 @@
 package httpclient
+
+import (
+	"github.com/cesc1802/core-service/config"
+	"github.com/go-resty/resty/v2"
+	"time"
+)
+
+func NewRestyClient(c config.Config, baseUrl string) (*resty.Client, error) {
+	// Create a Resty Client
+	client := resty.New()
+
+	// Unique settings at Client level
+	//--------------------------------
+	// Enable debug mode
+	client.SetDebug(c.Resty.Debug)
+
+	// Set client timeout as per your need
+	duration, err := time.ParseDuration(c.Resty.Timeout)
+	if err != nil {
+		return nil, err
+	}
+	client.SetTimeout(duration)
+
+	// You can override all below settings and options at request level if you want to
+	//--------------------------------------------------------------------------------
+	// Host URL for all request. So you can use relative URL in the request
+	client.SetHostURL(baseUrl)
+
+	// Headers for all request
+	client.SetHeader("Accept", "application/json")
+	client.SetHeaders(map[string]string{
+		"Content-Type": "application/json",
+	})
+
+	return client, nil
+}
